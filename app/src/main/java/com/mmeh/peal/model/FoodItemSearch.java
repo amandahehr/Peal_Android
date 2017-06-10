03880205ab23985e08647e47b9792a0dcc8d297f
William Takashi Mimura <mimurawil@gmail.com>
2017-06-10 11:42:13 -0700

    Adding some main classes. They are only shells, still need to code these.

---
+++ app/src/main/java/com/mmeh/peal/model/FoodItemSearch.java
@@ -0,0 +1,26 @@
+package com.mmeh.peal.model;
+
+public class FoodItemSearch {
+
+    public static FoodItem searchById(String itemId) {
+        //TODO: code this method - searchById
+        FoodItem foodItem = new FoodItem();
+        
+        return foodItem;
+    }
+
+    public static FoodItem searchByNDB(String itemNDB) {
+        //TODO: code this method - searchByNDB
+        FoodItem foodItem = new FoodItem();
+
+        return foodItem;
+    }
+
+    public static FoodItem[] searchByName(String itemName) {
+        //TODO: code this method - searchByName
+        FoodItem[] foodItems = new FoodItem[1];
+
+        return foodItems;
+    }
+
+}
