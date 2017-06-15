2ed45a192c30e0212a9e1fbcad4ae358a1b89f95
Amanda <amanhehr@gmail.com>
2017-06-14 17:19:24 -0700

    pages & menus, MonthCalendar causes crash

---
+++ app/src/main/java/com/mmeh/peal/RecipeBook.java
@@ -0,0 +1,51 @@
+package com.mmeh.peal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class RecipeBook extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Intent intent = new Intent();
+            int itemId = item.getItemId();
+            switch (item.getItemId()) {
+                case R.id.navigation_calendar:
+                    intent = new Intent(findViewById(itemId).getContext(), MonthCalendar.class);
+                    break;
+                case R.id.navigation_recipes:
+                    intent = new Intent(findViewById(itemId).getContext(), RecipeBook.class);
+                    break;
+                case R.id.navigation_shoppingList:
+                    intent = new Intent(findViewById(itemId).getContext(), ShoppingList.class);
+                    break;
+            }
+            startActivity(intent);
+            return true;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_book);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/com/mmeh/peal/ShoppingList.java
@@ -0,0 +1,54 @@
+package com.mmeh.peal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShoppingList extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Intent intent = new Intent();
+            int itemId = item.getItemId();
+            switch (item.getItemId()) {
+                case R.id.navigation_calendar:
+                    intent = new Intent(findViewById(itemId).getContext(), MonthCalendar.class);
+                    break;
+                case R.id.navigation_recipes:
+                    intent = new Intent(findViewById(itemId).getContext(), RecipeBook.class);
+                    break;
+                case R.id.navigation_shoppingList:
+                    intent = new Intent(findViewById(itemId).getContext(), ShoppingList.class);
+                    break;
+            }
+            startActivity(intent);
+            return true;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    }
+
+}
